a6c34a932a5a0a42807789ce4fdcbbba35365492
gargi singh <gargi.singh.9310@gmail.com>
2026-07-13 00:04:05 +0530

    mongodb_password

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
  export const connectDB= async () => {
-    await mongoose.connect('mongodb+srv://gargisingh9310_db_user:xaNwl1oHrGI95s8T@cluster0.82wheei.mongodb.net/Food-App').then(()=>console.log("connection successfully"));
+    await mongoose.connect('mongodb+srv://gargisingh9310_db_user:Gargi2708@cluster0.82wheei.mongodb.net/Food-App').then(()=>console.log("connection successfully"));
     
 }
